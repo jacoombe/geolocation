c126556d8852e78f39b30be22d8db3fbb028db93
James Coombes <jamespjcoombes@gmail.com>
2023-10-05 22:54:18 +0100

    pep 8 check and additional error

--- iplookup.py
+++ iplookup.py
@@ -1,23 +1,55 @@
+#!/usr/bin/env python3
+
+"""A command line tool to look up geolocation data"""
+
 import argparse
+import ipaddress
+import os
+import sys
+
 import requests
-KEY_URL = 'http://api.ipstack.com/'
+
 
 def get_ip_data(ip_address: str, key: str):
-    data = requests.get(f'http://api.ipstack.com/{ip_address}?access_key={key}')
+    data = requests.get(
+        f'http://api.ipstack.com/{ip_address}?access_key={key}'
+    )
     return data.json()
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-                    prog='IP Address Coordinate reported',
-                    description='Reports the coordinates of an IP address approximately')
+        prog='IP Address Coordinate reported',
+        description='Reports the coordinates of an IP address approximately'
+        'the first response is the lattitude the second response is the '
+        'longitude'
+    )
 
     parser.add_argument('ipaddress')
     args = parser.parse_args()
 
+    if not os.path.exists('.apikey'):
+        print('No API key found in .apikey')
+        sys.exit(1)
+
     with open('.apikey', 'r') as fh:
         api_key = fh.read()
-    
-    
 
-    print(api_key)
+    ip_text = args.ipaddress
+    try:
+        ip_address = ipaddress.ip_address(ip_text)
+    except ValueError:
+        print(f'the ip address {ip_text} isnt valid')
+        sys.exit(2)
+
+    if not ip_address.is_global:
+        print(
+            f'{ip_text} is not geographic only global not local or private'
+            ' ip addresses are valid for lookup'
+        )
+        sys.exit(3)
+
+    info = get_ip_data(ip_text, api_key)
+
+    print(info['latitude'])
+    print(info['longitude'])
