7cb5f5fea9120e86cf54b000a307e3fcb3f7cf3d
James Coombes <jamespjcoombes@gmail.com>
2023-10-06 00:07:39 +0100

    FIx pep08

--- iplookup.py
+++ iplookup.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     info = get_ip_data(ip_text, api_key)
 
     if 'error' in info:
-        ec  = info['error']['code']
+        ec = info['error']['code']
         print(f"error {ec} from ipstack.com: {info['error']['info']}")
         sys.exit(ec)
 
